467fa2159df0a01f342815622cb7fa869d258f13
Marcin Moscicki <marcin@moscicki.net>
2019-02-26 05:12:33 +0100

    * slight enhancements to Var * cosmetic scaladoc edits

--- src/main/scala/net/turambar/slang/funny/Fun.scala
+++ src/main/scala/net/turambar/slang/funny/Fun.scala
@@ -14,11 +14,10 @@ object Fun {
 		final val Fun2Args = new Specializable.Group(scala.Int, scala.Long, scala.Double)
 		final val Fun2Arg1 = Fun2Args
 		final val Fun2Arg2 = Fun2Args
-//		final val Fun2Arg2Types = new Specializable.Group(scala.Int, scala.Long, scala.Double)
 		final val Fun2Results = new Specializable.Group(scala.Unit, scala.Boolean, scala.Int, scala.Float, scala.Long, scala.Double)
 	}
 
-	def nameOf(f :Nothing=>Any, default :String) :String = f.toString match {
+	private def nameOf(f :Nothing=>Any, default :String) :String = f.toString match {
 		case "<function1>" => default
 		case s => s
 	}
@@ -32,7 +31,7 @@ object Fun {
 
 
 	@inline def compose[@specialized(Fun1Args)A, @specialized(Fun1Results) B, @specialized(Fun1Results) C]
-			(outer: B => C, inner: A => B): A => C =
+	                   (outer: B => C, inner: A => B): A => C =
 		(outer, inner) match {
 			case (fc: ComposableFun[B, C], _) => outer compose inner
 			case (_, gc :ComposableFun[A, B]) => gc andThen outer
@@ -40,13 +39,13 @@ object Fun {
 		}
 
 	@inline def compose[@specialized(Fun1Args)A, @specialized(Fun1Results) B, @specialized(Fun1Results) C]
-			(name :String)(outer: B => C, inner: A => B): A => C =
+	                   (name :String)(outer: B => C, inner: A => B): A => C =
 		new ComposedFun[A, B, C](inner, outer) {
 			override val toString = name
 		}
 
 	@inline def compose[@specialized(Fun1Args) A, @specialized(Fun1Results) B, @specialized(Fun1Results) C]
-			(outerName :String, innerName :String)(outer :B=>C, inner :A=>B) :A=>C =
+	                   (outerName :String, innerName :String)(outer :B=>C, inner :A=>B) :A=>C =
 		compose(ComposedFun.name(outerName, innerName))(outer, inner)
 
 
@@ -64,13 +63,13 @@ object Fun {
 
 
 
-	@inline final def errorfun[X, E <:Exception :ClassTag](exception :X => E) :X=>Nothing = new ComposableFun[X, Nothing] {
+	@inline final def errorfun[X, E <: Exception :ClassTag](exception :X => E) :X=>Nothing = new ComposableFun[X, Nothing] {
 
 		override def apply(v1: X): Nothing = throw exception(v1)
 
-		override def compose[A](g: (A) => X): (A) => Nothing = errorfun(exception compose g)
+		override def compose[A](g: A => X): A => Nothing = errorfun(exception compose g)
 
-		override def andThen[A](g: (Nothing) => A): (X) => A = this
+		override def andThen[A](g: Nothing => A): X => A = this
 
 		override def toString = "throw "+classTag[E].runtimeClass.getName
 	}
@@ -84,9 +83,9 @@ object Fun {
 
 
 	trait ComposableFun[@specialized(Fun1Args) -X, @specialized(Fun1Results) +Y] extends (X=>Y) {
-		override def compose[A](g: (A) => X): (A) => Y = new ComposedFun(g, this)
+		override def compose[A](g: A => X): A => Y = new ComposedFun(g, this)
 
-		override def andThen[A](g: (Y) => A): (X) => A = new ComposedFun(this, g)
+		override def andThen[A](g: Y => A): X => A = new ComposedFun(this, g)
 	}
 
 
@@ -94,7 +93,7 @@ object Fun {
 
 	final class NamedFun1[@specialized(Fun1Args) -X, @specialized(Fun1Results) +Y](name :String, f :X=>Y) extends ComposableFun[X, Y] {
 		override def apply(v1: X): Y = f(v1)
-		override def toString = name
+		override def toString :String = name
 	}
 
 
@@ -103,22 +102,21 @@ object Fun {
 		final override def apply(v1: X): X = v1
 
 		@unspecialized
-		override def compose[A](g: (A) => X): (A) => X = g
+		override def compose[A](g: A => X): A => X = g
 
 		@unspecialized
-		override def andThen[A](g: (X) => A): (X) => A = g
+		override def andThen[A](g: X => A): X => A = g
 
 		override def toString = "{x=>x}" //todo print specialization
 	}
 
 	final class IdFun[@specialized(Fun1Args) X] extends GenericIdentity[X] {
-//		override def apply(x: X): X = x
 
 		@unspecialized
-		override def compose[A](g: (A) => X): g.type = g
+		override def compose[A](g: A => X): g.type = g
 
 		@unspecialized
-		override def andThen[A](g: (X) => A): g.type = g
+		override def andThen[A](g: X => A): g.type = g
 	}
 
 	object IdFun {
@@ -143,16 +141,16 @@ object Fun {
 		override def apply(v1: X): Y = value
 
 		@scala.annotation.unspecialized
-		override def compose[A](g: (A) => X): A => Y = new ConstFun(value)
+		override def compose[A](g: A => X): A => Y = new ConstFun(value)
 
-		override def andThen[A](g: (Y) => A): X => A = new ConstFun(g(value))
+		override def andThen[A](g: Y => A): X => A = new ConstFun(g(value))
 
-		override def equals(that :Any) = that match {
+		override def equals(that :Any) :Boolean = that match {
 			case const :ConstFun[_, _] => (this eq const) || value==const.value
 			case _ => false
 		}
 
-		override def hashCode = value.hashCode
+		override def hashCode :Int = value.hashCode
 
 		override def toString = s"{_ => $value}"
 	}
@@ -164,14 +162,14 @@ object Fun {
 	{
 		override def apply(x: X): Z = outer(inner(x))
 
-		override def equals(that :Any) = that match {
+		override def equals(that :Any) :Boolean = that match {
 			case c :ComposedFun[_, _, _] => (c eq this) || c.inner==inner && c.outer==outer
 			case _ => false
 		}
 
-		override def hashCode = inner.hashCode * 31 + outer.hashCode
+		override def hashCode :Int = inner.hashCode * 31 + outer.hashCode
 
-		override def toString = ComposedFun.name(nameOf(outer, "f"), nameOf(inner, "g")) //s"<${nameOf(outer, "f")} * ${nameOf(inner, "g")}>"
+		override def toString :String = ComposedFun.name(nameOf(outer, "f"), nameOf(inner, "g")) //s"<${nameOf(outer, "f")} * ${nameOf(inner, "g")}>"
 
 		private def nameOf(f :Nothing=>Any, default :String) = f.toString match {
 			case "<function1>" => default
@@ -254,18 +252,18 @@ object Fun {
 		def apply(x :X) :Y = x
 
 		@scala.annotation.unspecialized
-		override def compose[A](g: (A) => X): (A) => Y = g
+		override def compose[A](g: A => X): A => Y = g
 
 		@scala.annotation.unspecialized
-		override def andThen[A](g: (Y) => A): (X) => A = g
+		override def andThen[A](g: Y => A): X => A = g
 
 		def ident :X => Y = idfun[X]
 
 		final implicit val <:< :X<:<Y = implicitly[X<:<X]
 
-		final implicit override def cocast[T[+A]] = CastFun[T[X], T[Y]]
+		final implicit override def cocast[T[+A]] :UpCast[T[X], T[Y]] = CastFun[T[X], T[Y]]
 
-		final implicit override def contracast[T[-A]] = CastFun[T[Y], T[X]]
+		final implicit override def contracast[T[-A]] :UpCast[T[Y], T[X]] = CastFun[T[Y], T[X]]
 
 		final implicit override def covariant[T[+A]] :T[X]<:<T[Y] = implicitly[T[X] <:< T[X]]
 
@@ -273,7 +271,7 @@ object Fun {
 
 		final implicit override def pair[T[-A, +B]] :T[Y, X] <:< T[X, Y] = implicitly[T[Y, X] <:< T[Y, X]]
 
-		override def toString = s"((_:X) :Y)"
+		override def toString = "((_:X) :Y)"
 	}
 
 	private final class IdentityCast[X] extends UpCast[X, X] with GenericIdentity[X] {
@@ -299,7 +297,7 @@ object Fun {
 				override def apply(v1: T1, v2: T2): R = f(v1, v2)
 
 				@unspecialized
-				override def curried: (T1) => (T2) => R = Fun(toString)(super.curried)
+				override def curried: T1 => T2 => R = Fun(toString)(super.curried)
 
 				@unspecialized
 				override def tupled: ((T1, T2)) => R = Fun(s"($toString)")(super.tupled)
