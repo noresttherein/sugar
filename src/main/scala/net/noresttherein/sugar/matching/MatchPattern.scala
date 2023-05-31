cb0b9c68aceb730ce637d2394e423acfcbd4394a
noresttherein <marcin@moscicki.net>
2023-05-31 19:26:36 +0200

    Boolean matching pattern skeleton/factory

--- src/main/scala/net/noresttherein/sugar/matching/MatchPattern.scala
+++ src/main/scala/net/noresttherein/sugar/matching/MatchPattern.scala
@@ -3,10 +3,10 @@ package net.noresttherein.sugar.matching
 
 import scala.reflect.ClassTag
 
-import net.noresttherein.sugar.extensions.{downcastTypeParamMethods, OptionExtension}
+import net.noresttherein.sugar.extensions.{OptionExtension, castingMethods, downcastTypeParamMethods}
 import net.noresttherein.sugar.matching.MatchPattern.SpecializedArgs
 import net.noresttherein.sugar.vars.Opt
-import net.noresttherein.sugar.vars.Opt.Got
+import net.noresttherein.sugar.vars.Opt.{Got, Lack}
 
 //implicits
 import net.noresttherein.sugar.extensions.classNameMethods
@@ -93,6 +93,25 @@ object MatchPattern { //todo: a constructor macro adapting a partial function li
 	def adapt[@specialized(SpecializedArgs) In, Out](f :In => Opt[Out]) :MatchPattern[In, Out] =
 		MatchPattern.adapt(s"MatchPattern(${f.innerClassName})")(f)
 
+	/** Adapt a given partial function to an extractor object usable in pattern matching.
+	  * @param name a textual identifier of the extractor, used in its `toString` implementation.
+	  * @param f    an extractor function.
+	  */
+	def when[@specialized(SpecializedArgs) In, Out](name :String)(f :PartialFunction[In, Out]) :MatchPattern[In, Out] =
+		new MatchPattern[In, Out] {
+			override def unapply(in :In) = {
+				val res = f.applyOrElse(in, fallback)
+				if (res.asAnyRef eq fallback) Lack
+				else Got(res.asInstanceOf[Out])
+			}
+			override def lift = f.lift
+			override def toString = "name"
+		}
+
+	/** Adapt a given partial function to an extractor object usable in pattern matching. */
+	def when[@specialized(SpecializedArgs) In, Out](f :PartialFunction[In, Out]) :MatchPattern[In, Out] =
+		when(s"MatchPattern(${f.innerClassName})")(f)
+
 	/** Creates a `MatchPattern` which always succeeds based on the given getter function.
 	  * @param name a textual identifier of the extractor, used in its `toString` implementation.
 	  * @param f    a getter function.
@@ -133,4 +152,74 @@ object MatchPattern { //todo: a constructor macro adapting a partial function li
 	}
 
 	final val SpecializedArgs = new Specializable.Group(Byte, Char, Int, Long, Float, Double)
+
+	final val Bool = BooleanMatchPattern
+
+	private[this] val fallback :Any => Any = _ => fallback
+}
+
+
+
+
+/** An skeleton for matching pattern objects which can be also used directly to verify if the argument
+  * matches this pattern. It is a single abstract method type (''SAM'') and thus
+  * the compiler will convert a lambda function `In => Boolean` where the type `BooleanMatchPattern[In]` is expected,
+  * eliminating the overhead of wrapping a function while preserving the convenience of the shortened definition.
+  * @tparam In type of matched (argument) values
+  */
+@SerialVersionUID(Ver)
+trait BooleanMatchPattern[@specialized(SpecializedArgs) -In] extends Serializable {
+	/** The single abstract method of this class, checking if the argument matches this pattern. */
+	def unapply(arg :In) :Boolean
+
+	/** Same as [[net.noresttherein.sugar.matching.MatchPattern.unapply unapply]]. Directly dispatches this call
+	  * to the latter. */
+	@inline final def apply(arg :In) :Boolean = unapply(arg)
+
+	override def toString: String = this.abbrevClassName
+}
+
+
+
+
+/** Factory object for [[net.noresttherein.sugar.matching.BooleanMatchPattern BooleanMatchPattern]] extractor objects.
+  */
+@SerialVersionUID(Ver)
+object BooleanMatchPattern {
+	/** Forces a function literal given as the argument
+	  * to be SAM-converted to a [[net.noresttherein.sugar.matching.BooleanMatchPattern BooleanMatchPattern]].
+	  */
+	@inline def apply[In](pattern :BooleanMatchPattern[In]) :BooleanMatchPattern[In] = pattern
+
+	/** Adapt a given option returning function to an extractor object usable in pattern matching.
+	  * @param name a textual identifier of the extractor, used in its `toString` implementation.
+	  * @param f    an extractor function.
+	  */
+	def adapt[@specialized(SpecializedArgs) In](name :String)(f :In => Boolean) :BooleanMatchPattern[In] =
+		new BooleanMatchPattern[In] {
+			override def unapply(in :In) = f(in)
+			override def toString = name
+		}
+
+	/** Adapt a given option returning function to an extractor object usable in pattern matching. */
+	def adapt[@specialized(SpecializedArgs) In](f :In => Boolean) :BooleanMatchPattern[In] =
+		adapt(s"BooleanMatchPattern(${f.innerClassName})")(f)
+
+	/** Adapt a given partial function to an extractor object usable in pattern matching.
+	  * The pattern will match if the function is both defined for the matched object, and returns `true`.
+	  * @param name a textual identifier of the extractor, used in its `toString` implementation.
+	  * @param f    an extractor function.
+	  */
+	def when[@specialized(SpecializedArgs) In](name :String)(f :PartialFunction[In, Boolean]) :BooleanMatchPattern[In] =
+		new BooleanMatchPattern[In] {
+			override def unapply(in :In) = f.applyOrElse(in, (_ :In) => false)
+			override def toString = "name"
+		}
+
+	/** Adapt a given partial function to an extractor object usable in pattern matching.
+	  * The pattern will match if the function is both defined for the matched object, and returns `true`.
+	  */
+	def when[@specialized(SpecializedArgs) In](f :PartialFunction[In, Boolean]) :BooleanMatchPattern[In] =
+		when(s"BooleanMatchPattern(${ f.innerClassName })")(f)
+
 }
