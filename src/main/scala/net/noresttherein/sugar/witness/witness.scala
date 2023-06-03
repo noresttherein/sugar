e3c3ae9977bc881790765941e3aed806e8b84587
noresttherein <marcin@moscicki.net>
2023-06-03 21:02:58 +0200

    SpecificConversion trick class for precedence of implicit conversions

--- src/main/scala/net/noresttherein/sugar/witness/witness.scala
+++ src/main/scala/net/noresttherein/sugar/witness/witness.scala
@@ -5,3 +5,25 @@ package net.noresttherein.sugar
 package object witness {
 	final val Ver = 1L
 }
+
+
+package witness {
+
+	/** A function class used for implicit conversions in order to force precedence of one definition over another,
+	  * despite having the same argument and return types.
+	  * Defining a conversion as `SpecificConversion[X, Y]` means it will be chosen (if in the implicit search scope,
+	  * or important) over any competing implicit definition of a `X => Y`, or an implicit method
+	  * with the same signature.
+	  */
+	trait SpecificConversion[-X, +Y] extends (X => Y)
+
+	object SpecificConversion {
+		/** Forces a SAM type promotion of a function literal `X => Y` to a `SpecificConversion[X, Y]` and immediately
+		  * returns it.
+		  */
+		def apply[X, Y](conversion :SpecificConversion[X, Y]) :conversion.type = conversion
+
+		/** Promotes a function to a `SpecificConversion[X, Y]`. */
+		def wrap[X, Y](conversion :X => Y) :SpecificConversion[X, Y] = conversion(_)
+	}
+}
