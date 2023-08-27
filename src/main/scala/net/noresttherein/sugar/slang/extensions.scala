993ea0f60f64d1c59ad7cf44475f04a656a3b57d
noresttherein <marcin@moscicki.net>
2023-08-27 20:09:20 +0200

    identityHashCode extension method (moved from reflect)

---
+++ src/main/scala/net/noresttherein/sugar/slang/extensions.scala
@@ -0,0 +1,22 @@
+package net.noresttherein.sugar.slang
+
+import net.noresttherein.sugar.slang.extensions.AnyRefExtension
+
+
+
+
+
+trait extensions extends Any {
+	/** Adds `identityHashCode` method to all reference types. */
+	@inline implicit final def AnyRefExtension(self :AnyRef) :AnyRefExtension = new AnyRefExtension(self)
+}
+
+
+
+@SerialVersionUID(Ver)
+object extensions {
+	class AnyRefExtension(private val self :AnyRef) extends AnyVal {
+		/** The value of `hashCode` as it would be inherited for this object from the default implementation in `AnyRef`. */
+		@inline def identityHashCode :Int = System.identityHashCode(self)
+	}
+}
---
+++ src/main/scala/net/noresttherein/sugar/slang/slang.scala
@@ -0,0 +1,8 @@
+package net.noresttherein.sugar
+
+
+
+
+package object slang {
+	private[slang] final val Ver = 1L
+}
