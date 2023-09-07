b27cd86089f7efc8838b995a61a2a899920b7329
noresttherein <marcin@moscicki.net>
2023-09-08 00:40:16 +0200

    Replaced reduce with fold as the primary implementation

--- src/main/scala/net/noresttherein/sugar/collections/ArrayLikeOps.scala
+++ src/main/scala/net/noresttherein/sugar/collections/ArrayLikeOps.scala
@@ -15,7 +15,7 @@ import net.noresttherein.sugar.extensions.cast2TypeParamsMethods
   */
 private object ArrayLikeOps {
 	def foreach[E, U](array :Array[E], from :Int, until :Int)(f :E => U) :Unit = {
-		def forEach[@specialized(Everything) X](arr :Array[X])(f :X => U) :Unit = {
+		def foreach[@specialized(Everything) X](arr :Array[X])(f :X => U) :Unit = {
 			var i = from
 			while (i < until) {
 				f(arr(i))
@@ -24,20 +24,21 @@ private object ArrayLikeOps {
 		}
 		val g = f.asInstanceOf[Any => U]
 		array match {
-			case a :Array[AnyRef]   => forEach(a)(g)
-			case a :Array[Int]      => forEach(a)(g)
-			case a :Array[Long]     => forEach(a)(g)
-			case a :Array[Double]   => forEach(a)(g)
-			case a :Array[Char]     => forEach(a)(g)
-			case a :Array[Byte]     => forEach(a)(g)
-			case a :Array[Float]    => forEach(a)(g)
-			case a :Array[Short]    => forEach(a)(g)
-			case a :Array[Boolean]  => forEach(a)(g)
+			case a :Array[AnyRef]   => foreach(a)(g)
+			case a :Array[Int]      => foreach(a)(g)
+			case a :Array[Long]     => foreach(a)(g)
+			case a :Array[Double]   => foreach(a)(g)
+			case a :Array[Char]     => foreach(a)(g)
+			case a :Array[Byte]     => foreach(a)(g)
+			case a :Array[Float]    => foreach(a)(g)
+			case a :Array[Short]    => foreach(a)(g)
+			case a :Array[Boolean]  => foreach(a)(g)
+			case null               => throw new NullPointerException()
 		}
 	}
 	
 	def indexOf[E](array :Array[_ <: E], offset :Int, length :Int)(elem :E, from :Int) :Int = {
-		def index[@specialized(Everything) X](arr :Array[X], elem :Any, from :Int) :Int = {
+		def indexOf[@specialized(Everything) X](arr :Array[X], elem :Any, from :Int) :Int = {
 			val end = offset + length
 			var i = offset + math.max(from, 0)
 			if (array.getClass.getComponentType isAssignableFrom Unbox(elem.getClass)) {
@@ -52,20 +53,21 @@ private object ArrayLikeOps {
 		if (from >= length)
 			-1
 		else array match {
-			case a :Array[AnyRef]  => index(a, elem, from)
-			case a :Array[Int]     => index(a, elem, from)
-			case a :Array[Long]    => index(a, elem, from)
-			case a :Array[Double]  => index(a, elem, from)
-			case a :Array[Char]    => index(a, elem, from)
-			case a :Array[Byte]    => index(a, elem, from)
-			case a :Array[Float]   => index(a, elem, from)
-			case a :Array[Short]   => index(a, elem, from)
-			case a :Array[Boolean] => index(a, elem, from)
+			case a :Array[AnyRef]  => indexOf(a, elem, from)
+			case a :Array[Int]     => indexOf(a, elem, from)
+			case a :Array[Long]    => indexOf(a, elem, from)
+			case a :Array[Double]  => indexOf(a, elem, from)
+			case a :Array[Char]    => indexOf(a, elem, from)
+			case a :Array[Byte]    => indexOf(a, elem, from)
+			case a :Array[Float]   => indexOf(a, elem, from)
+			case a :Array[Short]   => indexOf(a, elem, from)
+			case a :Array[Boolean] => indexOf(a, elem, from)
+			case null               => throw new NullPointerException()
 		}
 	}
 
 	def lastIndexOf[E](array :Array[_ <: E], offset :Int, length :Int)(elem :E, end :Int) :Int = {
-		def lastIndex[@specialized(Everything) X](arr :Array[X], elem :Any, start :Int) :Int = {
+		def lastIndexOf[@specialized(Everything) X](arr :Array[X], elem :Any, start :Int) :Int = {
 			var i = offset + start
 			if (array.getClass.getComponentType isAssignableFrom Unbox(elem.getClass)) {
 				val x = elem.asInstanceOf[X]
@@ -79,20 +81,21 @@ private object ArrayLikeOps {
 		if (end < 0)
 			-1
 		else array match {
-			case a :Array[AnyRef]  => lastIndex(a, elem, math.min(end, length - 1))
-			case a :Array[Int]     => lastIndex(a, elem, math.min(end, length - 1))
-			case a :Array[Long]    => lastIndex(a, elem, math.min(end, length - 1))
-			case a :Array[Double]  => lastIndex(a, elem, math.min(end, length - 1))
-			case a :Array[Char]    => lastIndex(a, elem, math.min(end, length - 1))
-			case a :Array[Byte]    => lastIndex(a, elem, math.min(end, length - 1))
-			case a :Array[Float]   => lastIndex(a, elem, math.min(end, length - 1))
-			case a :Array[Short]   => lastIndex(a, elem, math.min(end, length - 1))
-			case a :Array[Boolean] => lastIndex(a, elem, math.min(end, length - 1))
+			case a :Array[AnyRef]  => lastIndexOf(a, elem, math.min(end, length - 1))
+			case a :Array[Int]     => lastIndexOf(a, elem, math.min(end, length - 1))
+			case a :Array[Long]    => lastIndexOf(a, elem, math.min(end, length - 1))
+			case a :Array[Double]  => lastIndexOf(a, elem, math.min(end, length - 1))
+			case a :Array[Char]    => lastIndexOf(a, elem, math.min(end, length - 1))
+			case a :Array[Byte]    => lastIndexOf(a, elem, math.min(end, length - 1))
+			case a :Array[Float]   => lastIndexOf(a, elem, math.min(end, length - 1))
+			case a :Array[Short]   => lastIndexOf(a, elem, math.min(end, length - 1))
+			case a :Array[Boolean] => lastIndexOf(a, elem, math.min(end, length - 1))
+			case null               => throw new NullPointerException()
 		}
 	}
 
 	def lastIndexWhere[E](array :Array[E], offset :Int, length :Int)(p :E => Boolean, end :Int) :Int = {
-		def lastIdx[@specialized(Everything) X](arr :Array[X], p :X => Boolean, from :Int, until :Int) :Int = {
+		def lastIndexWhere[@specialized(Everything) X](arr :Array[X], p :X => Boolean, from :Int, until :Int) :Int = {
 			var i = until
 			while (i >= from && !p(arr(i)))
 				i -= 1
@@ -102,34 +105,68 @@ private object ArrayLikeOps {
 		if (end < 0)
 			-1
 		else array match {
-			case a :Array[AnyRef]  => lastIdx(a, p.asInstanceOf[AnyRef => Boolean], offset, start)
-			case a :Array[Int]     => lastIdx(a, p.asInstanceOf[Int => Boolean], offset, start)
-			case a :Array[Long]    => lastIdx(a, p.asInstanceOf[Long => Boolean], offset, start)
-			case a :Array[Double]  => lastIdx(a, p.asInstanceOf[Double => Boolean], offset, start)
-			case a :Array[Char]    => lastIdx(a, p.asInstanceOf[Char => Boolean], offset, start)
-			case a :Array[Byte]    => lastIdx(a, p.asInstanceOf[Byte => Boolean], offset, start)
-			case a :Array[Float]   => lastIdx(a, p.asInstanceOf[Float => Boolean], offset, start)
-			case a :Array[Short]   => lastIdx(a, p.asInstanceOf[Short => Boolean], offset, start)
-			case a :Array[Boolean] => lastIdx(a, p.asInstanceOf[Boolean => Boolean], offset, start)
+			case a :Array[AnyRef]  => lastIndexWhere(a, p.asInstanceOf[AnyRef => Boolean], offset, start)
+			case a :Array[Int]     => lastIndexWhere(a, p.asInstanceOf[Int => Boolean], offset, start)
+			case a :Array[Long]    => lastIndexWhere(a, p.asInstanceOf[Long => Boolean], offset, start)
+			case a :Array[Double]  => lastIndexWhere(a, p.asInstanceOf[Double => Boolean], offset, start)
+			case a :Array[Char]    => lastIndexWhere(a, p.asInstanceOf[Char => Boolean], offset, start)
+			case a :Array[Byte]    => lastIndexWhere(a, p.asInstanceOf[Byte => Boolean], offset, start)
+			case a :Array[Float]   => lastIndexWhere(a, p.asInstanceOf[Float => Boolean], offset, start)
+			case a :Array[Short]   => lastIndexWhere(a, p.asInstanceOf[Short => Boolean], offset, start)
+			case a :Array[Boolean] => lastIndexWhere(a, p.asInstanceOf[Boolean => Boolean], offset, start)
+			case null               => throw new NullPointerException()
 		}
 	}
 
 	def indexWhere[E](array :Array[E], offset :Int, length :Int)(p :E => Boolean, from :Int) :Int =
-		if (from > length)
+		if (from >= length)
 			-1
+//		else
+//			indexWhere(array, offset + math.max(from, 0), offset + length, false)(p)
 		else {
 			val from0 = math.max(from, 0)
-			val res = segmentLength(array, p, offset + from0, offset + length, true)
+			val res = segmentLength(array, offset + from0, offset + length, true)(p)
 			if (res == length - from0) -1 else res + from0
 		}
 
-	@inline def segmentLength[E](array :Array[E], offset :Int, length :Int)(p :E => Boolean, from :Int) :Int =
-		if (from > length)
-			0
-		else
-			segmentLength(array, p, offset + math.max(from, 0), offset + length)
+/*
+	def indexWhere[E](array :Array[E], from :Int, until :Int, flipped :Boolean)(p :E => Boolean) :Int = {
+		def indexWhere[@specialized(Everything) X](arr :Array[X], p :X => Boolean) :Int = {
+			var i = from
+			while (i < until && p(arr(i)) == flipped)
+				i += 1
+			i - from
+		}
+		val length = array.length
+		if (from >= until | until < 0 || from >= length)
+			-1
+		else array match {
+			case a :Array[AnyRef]  => indexWhere(a, p.asInstanceOf[AnyRef => Boolean])
+			case a :Array[Int]     => indexWhere(a, p.asInstanceOf[Int => Boolean])
+			case a :Array[Long]    => indexWhere(a, p.asInstanceOf[Long => Boolean])
+			case a :Array[Double]  => indexWhere(a, p.asInstanceOf[Double => Boolean])
+			case a :Array[Char]    => indexWhere(a, p.asInstanceOf[Char => Boolean])
+			case a :Array[Byte]    => indexWhere(a, p.asInstanceOf[Byte => Boolean])
+			case a :Array[Float]   => indexWhere(a, p.asInstanceOf[Float => Boolean])
+			case a :Array[Short]   => indexWhere(a, p.asInstanceOf[Short => Boolean])
+			case a :Array[Boolean] => indexWhere(a, p.asInstanceOf[Boolean => Boolean])
+			case null               => throw new NullPointerException()
+		}
+	}
+*/
 
-	def segmentLength[E](array :Array[E], p :E => Boolean, from :Int, until :Int, flipped :Boolean = false) :Int = {
+	@inline def segmentLength[E](array :Array[E], offset :Int, length :Int)(p :E => Boolean, from :Int) :Int =
+		if (from >= length)
+			0
+//		else {
+//			val from0 = offset + math.max(from, 0)
+//			val idx   = indexWhere(array, from0, offset + length, true)(p)
+//			if (idx < 0) length - from0 else idx
+//		}
+		else
+			segmentLength(array, offset + math.max(from, 0), offset + length, false)(p)
+
+	def segmentLength[E](array :Array[E], from :Int, until :Int, flipped :Boolean)(p :E => Boolean) :Int = {
 		def segmentLen[@specialized(Everything) X](arr :Array[X], p :X => Boolean) :Int = {
 			var i = from
 			while (i < until && p(arr(i)) != flipped)
@@ -149,8 +186,39 @@ private object ArrayLikeOps {
 			case a :Array[Float]   => segmentLen(a, p.asInstanceOf[Float => Boolean])
 			case a :Array[Short]   => segmentLen(a, p.asInstanceOf[Short => Boolean])
 			case a :Array[Boolean] => segmentLen(a, p.asInstanceOf[Boolean => Boolean])
+			case null               => throw new NullPointerException()
 		}
 	}
+/*
+	def forall[E](array :Array[E], from :Int, until :Int, flipped :Boolean = false)(f :E => Boolean) :Boolean = {
+		val length = array.length
+		until <= from | until <= 0 || from >= length || {
+			def forall[@specialized(Everything) X](a :Array[X], f :X => Boolean) :Boolean = {
+				val until0 = math.min(until, length)
+				var i      = math.max(from, 0)
+				while (i < until0 && f(a(i)) != flipped)
+					i += 1
+				i == until0
+			}
+			(array :Array[_]) match {
+				case a :Array[AnyRef]   => forall(a, f.castParam1[AnyRef])
+				case a :Array[Int]      => forall(a, f.castParam1[Int])
+				case a :Array[Long]     => forall(a, f.castParam1[Long])
+				case a :Array[Double]   => forall(a, f.castParam1[Double])
+				case a :Array[Char]     => forall(a, f.castParam1[Char])
+				case a :Array[Byte]     => forall(a, f.castParam1[Byte])
+				case a :Array[Float]    => forall(a, f.castParam1[Float])
+				case a :Array[Short]    => forall(a, f.castParam1[Short])
+				case a :Array[Boolean]  => forall(a, f.castParam1[Boolean])
+				case null               => throw new NullPointerException()
+			}
+		}
+	}
+*/
+//
+//	def forall[E](array :Array[E], from :Int, until :Int)(f :E => Boolean) :Boolean =
+//		indexWhere(array, from, until)(f, 0) == -1
+//
 
 	def updateAll[E](array :Array[E], from :Int, until :Int)(f :Int => E) :Unit = {
 		val length = array.length
@@ -174,61 +242,77 @@ private object ArrayLikeOps {
 				case a :Array[Float]    => fill(a, f.castParam2[Float])
 				case a :Array[Short]    => fill(a, f.castParam2[Short])
 				case a :Array[Boolean]  => fill(a, f.castParam2[Boolean])
+				case null               => throw new NullPointerException()
 			}
 		}
 
 	}
 
-	def reduceLeft[A >: E, E](array :Array[E])(op :(A, E) => A) :A = {
-		if (array.length == 0)
+	def foldLeft[A, E](array :Array[E], from :Int, until :Int)(z :A)(op :(A, E) => A) :A = 
+		if (until <= from)
+			z
+		else {
+			def foldl[@specialized(Specializable.Everything) X](arr :Array[X], op :(A, X) => A) :A = {
+				var i   = from
+				var res = z
+				while (i < until) {
+					res = op(res, arr(i))
+					i  += 1
+				}
+				res
+			}
+			(array :Array[_]) match {
+				case a :Array[AnyRef]  => foldl(a, op.asInstanceOf[(A, AnyRef) => A])
+				case a :Array[Int]     => foldl(a, op.asInstanceOf[(A, Int) => A])
+				case a :Array[Long]    => foldl(a, op.asInstanceOf[(A, Long) => A])
+				case a :Array[Double]  => foldl(a, op.asInstanceOf[(A, Double) => A])
+				case a :Array[Byte]    => foldl(a, op.asInstanceOf[(A, Byte) => A])
+				case a :Array[Char]    => foldl(a, op.asInstanceOf[(A, Char) => A])
+				case a :Array[Float]   => foldl(a, op.asInstanceOf[(A, Float) => A])
+				case a :Array[Short]   => foldl(a, op.asInstanceOf[(A, Short) => A])
+				case a :Array[Boolean] => foldl(a, op.asInstanceOf[(A, Boolean) => A])
+				case null               => throw new NullPointerException()
+			}
+	}
+
+	def foldRight[E, A](array :Array[E], from :Int, until :Int)(z :A)(op :(E, A) => A) :A =
+		if (until <= from)
+			z
+		else {
+			def foldr[@specialized(Specializable.Everything) X](arr :Array[X], op :(X, A) => A) :A = {
+				var i   = until
+				var res = z
+				while (i > from) {
+					i  -= 1
+					res = op(arr(i), res)
+				}
+				res
+			}
+			(array :Array[_]) match {
+				case a :Array[AnyRef]  => foldr(a, op.asInstanceOf[(AnyRef, A) => A])
+				case a :Array[Int]     => foldr(a, op.asInstanceOf[(Int, A) => A])
+				case a :Array[Long]    => foldr(a, op.asInstanceOf[(Long, A) => A])
+				case a :Array[Double]  => foldr(a, op.asInstanceOf[(Double, A) => A])
+				case a :Array[Byte]    => foldr(a, op.asInstanceOf[(Byte, A) => A])
+				case a :Array[Char]    => foldr(a, op.asInstanceOf[(Char, A) => A])
+				case a :Array[Float]   => foldr(a, op.asInstanceOf[(Float, A) => A])
+				case a :Array[Short]   => foldr(a, op.asInstanceOf[(Short, A) => A])
+				case a :Array[Boolean] => foldr(a, op.asInstanceOf[(Boolean, A) => A])
+				case null               => throw new NullPointerException
+			}
+	}
+	
+	def reduceLeft[A >: E, E](array :Array[E], from :Int, until :Int)(op :(A, E) => A) :A =
+		if (until <= from)
 			throw new UnsupportedOperationException("ArrayLike().reduceLeft")
-		def reducel[@specialized(Specializable.Everything) X](arr :Array[X], op :(Any, X) => Any) :A = {
-			var i = 1; val len = arr.length
-			var res :Any = arr(0)
-			while (i < len) {
-				res = op(res, arr(i))
-				i += 1
-			}
-			res.asInstanceOf[A]
-		}
-		(array :Array[_]) match {
-			case a :Array[AnyRef]  => reducel(a, op.asInstanceOf[(Any, AnyRef) => Any])
-			case a :Array[Int]     => reducel(a, op.asInstanceOf[(Any, Int) => Any])
-			case a :Array[Long]    => reducel(a, op.asInstanceOf[(Any, Long) => Any])
-			case a :Array[Double]  => reducel(a, op.asInstanceOf[(Any, Double) => Any])
-			case a :Array[Byte]    => reducel(a, op.asInstanceOf[(Any, Byte) => Any])
-			case a :Array[Char]    => reducel(a, op.asInstanceOf[(Any, Char) => Any])
-			case a :Array[Float]   => reducel(a, op.asInstanceOf[(Any, Float) => Any])
-			case a :Array[Short]   => reducel(a, op.asInstanceOf[(Any, Short) => Any])
-			case a :Array[Boolean] => reducel(a, op.asInstanceOf[(Any, Boolean) => Any])
-		}
-	}
+		else
+			foldLeft[A, E](array, from + 1, until)(array(from))(op)
 
-	def reduceRight[E, A >: E](array :Array[E])(op :(E, A) => A) :A = {
-		if (array.length == 0)
+	def reduceRight[E, A >: E](array :Array[E], from :Int, until :Int)(op :(E, A) => A) :A =
+		if (until <= from)
 			throw new UnsupportedOperationException("ArrayLike().reduceRight")
-		def reducer[@specialized(Specializable.Everything) X](arr :Array[X], op :(X, Any) => Any) :A = {
-			val len = arr.length
-			var i = len - 1
-			var res :Any = arr(i)
-			while (i > 0) {
-				i -= 1
-				res = op(arr(i), res)
-			}
-			res.asInstanceOf[A]
-		}
-		(array :Array[_]) match {
-			case a :Array[AnyRef]  => reducer(a, op.asInstanceOf[(AnyRef, Any) => Any])
-			case a :Array[Int]     => reducer(a, op.asInstanceOf[(Int, Any) => Any])
-			case a :Array[Long]    => reducer(a, op.asInstanceOf[(Long, Any) => Any])
-			case a :Array[Double]  => reducer(a, op.asInstanceOf[(Double, Any) => Any])
-			case a :Array[Byte]    => reducer(a, op.asInstanceOf[(Byte, Any) => Any])
-			case a :Array[Char]    => reducer(a, op.asInstanceOf[(Char, Any) => Any])
-			case a :Array[Float]   => reducer(a, op.asInstanceOf[(Float, Any) => Any])
-			case a :Array[Short]   => reducer(a, op.asInstanceOf[(Short, Any) => Any])
-			case a :Array[Boolean] => reducer(a, op.asInstanceOf[(Boolean, Any) => Any])
-		}
-	}
+		else
+			foldRight[E, A](array, from, until - 1)(array(until - 1))(op)
 
 	/** Maps all classes representable in `JVM` to their boxed representations. */
 	private[this] val Unbox = Map[Class[_], Class[_]](
