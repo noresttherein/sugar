777ef16ac011ac766bf24c7de3c0ede2a82330b3
noresttherein <marcin@moscicki.net>
2023-12-23 18:15:01 +0100

    ReversedIndexedBuffer

--- src/main/scala/net/noresttherein/sugar/collections/ReversedSeq.scala
+++ src/main/scala/net/noresttherein/sugar/collections/ReversedSeq.scala
@@ -1,6 +1,8 @@
 package net.noresttherein.sugar.collections
 
+import scala.annotation.tailrec
 import scala.collection.mutable
+import scala.collection.mutable.IndexedBuffer
 
 import net.noresttherein.sugar.collections.IndexedIterable.ApplyPreferred
 
@@ -8,30 +10,33 @@ import net.noresttherein.sugar.collections.IndexedIterable.ApplyPreferred
 
 
 /** A view of an `IndexedSeq` reversing the order of elements. */
+@SerialVersionUID(Ver)
 private sealed class ReversedSeq[+E](underlying :collection.IndexedSeq[E])
 	extends collection.AbstractSeq[E] with collection.IndexedSeq[E] with SlicingOps[E, collection.IndexedSeq[E]]
 {
-	private[this] val len = underlying.length
-	override def length :Int = len
+//	private[this] val len = underlying.length
+//	override def length :Int = len
+	override def length = underlying.length
 
 	override def apply(i :Int) :E =
-		if (i < 0) throw new IndexOutOfBoundsException(i.toString + " out of " + len)
-		else underlying(len - i - 1)
+		if (i < 0) throw new IndexOutOfBoundsException(i.toString + " out of " + underlying.length)
+		else underlying(underlying.length - i - 1)
 
 	protected override def trustedSlice(from :Int, until :Int) :collection.IndexedSeq[E] =
-		if (HasFastSlice(underlying))
+		if (HasFastSlice(underlying)) {
+			val len = underlying.length
 			new ReversedSeq(underlying.slice(len - until, len - from))
-		else
+		} else
 			super[IndexedSeq].slice(from, until)
 
 	//todo: test this
 	override def segmentLength(p :E => Boolean, from :Int) :Int =
-		if (from >= len)
+		if (from >= underlying.length)
 			0
 		else if (ApplyPreferred(underlying))
 			super[IndexedSeq].segmentLength(p, from)
 		else {
-			val until = len - math.max(from, 0)
+			val until = underlying.length - math.max(from, 0)
 			val i = underlying.lastIndexWhere(!p(_), until - 1)
 			until - i - 1
 		}
@@ -44,36 +49,98 @@ private sealed class ReversedSeq[+E](underlying :collection.IndexedSeq[E])
 }
 
 
+@SerialVersionUID(Ver)
 private object ReversedSeq {
 	def apply[E](seq :collection.IndexedSeq[E]) :collection.IndexedSeq[E] = seq match {
-		case seq :IndexedSeq[E]         => new Immutable(seq)
-		case seq :mutable.IndexedSeq[E] => new Mutable(seq)
+		case seq :IndexedSeq[E]         => new ImmutableReversedSeq(seq)
+		case seq :mutable.IndexedSeq[E] => new MutableReversedSeq(seq)
 		case _                          => new ReversedSeq(seq)
 	}
-	def apply[E](seq :IndexedSeq[E]) :IndexedSeq[E] = new Immutable(seq)
+	def apply[E](seq :IndexedSeq[E]) :IndexedSeq[E] = new ImmutableReversedSeq(seq)
+	def apply[E](seq :mutable.IndexedSeq[E]) :mutable.IndexedSeq[E] = new MutableReversedSeq(seq)
+}
 
-	private class Immutable[+E](override val reverse :IndexedSeq[E])
-		extends ReversedSeq[E](reverse) with IndexedSeq[E] with SlicingOps[E, IndexedSeq[E]]
-	{
-		protected override def trustedSlice(from :Int, until :Int) :IndexedSeq[E] =
-			if (HasFastSlice(reverse))
-				new Immutable(reverse.slice(length - until, length - from))
-			else
-				super[IndexedSeq].slice(from, until)
+
+@SerialVersionUID(Ver)
+private class ImmutableReversedSeq[+E](override val reverse :IndexedSeq[E])
+	extends ReversedSeq[E](reverse) with IndexedSeq[E] with SlicingOps[E, IndexedSeq[E]]
+{
+	protected override def trustedSlice(from :Int, until :Int) :IndexedSeq[E] =
+		if (HasFastSlice(reverse))
+			new ImmutableReversedSeq(reverse.slice(length - until, length - from))
+		else
+			super[IndexedSeq].slice(from, until)
+}
+
+
+@SerialVersionUID(Ver)
+private class MutableReversedSeq[E](override val reverse :mutable.IndexedSeq[E])
+	extends ReversedSeq[E](reverse) with mutable.IndexedSeq[E] with SlicingOps[E, mutable.IndexedSeq[E]]
+{
+	override def update(idx :Int, elem :E) :Unit =
+		if (idx < 0) throw new IndexOutOfBoundsException(idx.toString + " out of " + length)
+		else reverse(length - idx - 1) = elem
+
+	protected override def trustedSlice(from :Int, until :Int) :mutable.IndexedSeq[E] =
+		if (HasFastSlice(reverse))
+			new MutableReversedSeq(reverse.slice(length - until, length - from))
+		else
+			super[IndexedSeq].slice(from, until)
+}
+
+
+
+
+@SerialVersionUID(Ver)
+private class ReversedIndexedBuffer[E](override val reverse :IndexedBuffer[E])
+	extends MutableReversedSeq[E](reverse) with IndexedBuffer[E] with SlicingOps[E, IndexedBuffer[E]]
+{
+	protected override def trustedSlice(from :Int, until :Int) :IndexedBuffer[E] =
+		if (HasFastSlice(reverse)) {
+			val len = reverse.length
+			new ReversedIndexedBuffer(reverse.slice(len - until, len - from))
+		} else
+			super[IndexedBuffer].slice(from, until)
+
+	override def addOne(elem :E) :this.type = { reverse.prepend(elem); this }
+	override def prepend(elem :E) :this.type = { reverse.addOne(elem); this }
+	override def addAll(xs :IterableOnce[E]) :this.type = { reverse.prependAll(reverseOther(xs)); this }
+	override def prependAll(elems :IterableOnce[E]) :this.type = { reverse.addAll(reverseOther(elems)); this }
+
+	override def insert(idx :Int, elem :E) :Unit =
+		if (idx < 0)
+			throw new IndexOutOfBoundsException(idx + " out of " + length)
+		else
+			reverse.insert(reverse.length - idx, elem)
+
+	override def insertAll(idx :Int, elems :IterableOnce[E]) :Unit = {
+		reverse.insertAll(reverse.length - idx, reverseOther(elems))
+		this
 	}
 
-	private class Mutable[E](override val reverse :mutable.IndexedSeq[E])
-		extends ReversedSeq[E](reverse) with mutable.IndexedSeq[E] with SlicingOps[E, mutable.IndexedSeq[E]]
-	{
-		override def update(idx :Int, elem :E) :Unit =
-			if (idx < 0) throw new IndexOutOfBoundsException(idx.toString + " out of " + length)
-			else reverse(length - idx - 1) = elem
+	@tailrec final override def patchInPlace(from :Int, patch :IterableOnce[E], replaced :Int) :this.type =
+		if (from <= 0)
+			patchInPlace(0, patch, replaced)
+		else if (from > reverse.length)
+			patchInPlace(reverse.length, patch, replaced)
+		else {
+			reverse.patchInPlace(reverse.length - from, reverseOther(patch), replaced)
+			this
+		}
 
-		protected override def trustedSlice(from :Int, until :Int) :mutable.IndexedSeq[E] =
-			if (HasFastSlice(reverse))
-				new Mutable(reverse.slice(length - until, length - from))
-			else
-				super[IndexedSeq].slice(from, until)
+	override def remove(idx :Int) :E =
+		if (idx < 0) throw new IndexOutOfBoundsException(idx + " out of " + length)
+		else reverse.remove(reverse.length - 1 - idx)
 
+	override def remove(idx :Int, count :Int) :Unit =
+		if (idx < 0) throw new IndexOutOfBoundsException(idx + " out of " + length)
+		else reverse.remove(reverse.length - idx, count)
+
+	override def clear() :Unit = reverse.clear()
+
+	private def reverseOther(elems :IterableOnce[E]) :IterableOnce[E] = elems match {
+		case seq :ReversedSeq[E] => seq.reverse
+		case seq :collection.IndexedSeq[E] => ReversedSeq(seq)
+		case _ => util.reverse(elems)
 	}
 }
