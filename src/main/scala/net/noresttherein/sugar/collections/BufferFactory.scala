77bac6630bd718b3568aed7871f846dbcbff872b
noresttherein <marcin@moscicki.net>
2023-12-24 02:24:08 +0100

    Added a delegate BufferFactory creating a plain ArrayBuffer.

--- src/main/scala/net/noresttherein/sugar/collections/BufferFactory.scala
+++ src/main/scala/net/noresttherein/sugar/collections/BufferFactory.scala
@@ -1,7 +1,7 @@
 package net.noresttherein.sugar.collections
 
 import scala.collection.SeqFactory
-import scala.collection.mutable.{Buffer, Builder, GrowableBuilder}
+import scala.collection.mutable.{ArrayBuffer, Buffer, Builder, GrowableBuilder}
 
 
 
@@ -9,7 +9,7 @@ import scala.collection.mutable.{Buffer, Builder, GrowableBuilder}
 /**
   * @author Marcin Mościcki
   */
-trait BufferFactory[+C[A] <: Buffer[A] with collection.SeqOps[A, Buffer, Buffer[A]]] extends SeqFactory[C] {
+trait BufferFactory[+C[A] <: Buffer[A]] extends SeqFactory[C] {
 	/** A new, empty buffer. Same as `empty`, but slightly more succinct, and puts emphasis on the element type. */
 	@inline final def of[E] :C[E] = empty[E]
 
@@ -21,4 +21,18 @@ trait BufferFactory[+C[A] <: Buffer[A] with collection.SeqOps[A, Buffer, Buffer[
 	override def from[E](source :IterableOnce[E]) :C[E] = empty[E] ++= source
 
 	override def newBuilder[E] :Builder[E, C[E]] = new GrowableBuilder[E, C[E]](empty)
+}
+
+
+@SerialVersionUID(Ver)
+private object ArrayBufferFactory extends BufferFactory[ArrayBuffer] {
+	override def ofCapacity[E](capacity :Int) :ArrayBuffer[E] = new ArrayBuffer(capacity)
+	override def empty[A] :ArrayBuffer[A] = new ArrayBuffer
+}
+
+@SerialVersionUID(Ver)
+private class BufferFactoryAdapter[C[A] <: Buffer[A]](factory :SeqFactory[C])
+	extends SeqFactory.Delegate[C](factory) with BufferFactory[C]
+{
+	override def ofCapacity[E](capacity :Int) :C[E] = empty
 }
