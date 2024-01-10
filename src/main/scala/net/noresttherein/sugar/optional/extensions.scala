295b960efbcf61d91ba6e36f93a094b24626b7b3
noresttherein <marcin@moscicki.net>
2024-01-10 04:06:56 +0100

    Moved ensuring extension methods to package slang.

--- src/main/scala/net/noresttherein/sugar/optional/extensions.scala
+++ src/main/scala/net/noresttherein/sugar/optional/extensions.scala
@@ -2,10 +2,8 @@ package net.noresttherein.sugar.optional
 
 import scala.reflect.ClassTag
 
-import net.noresttherein.sugar.extensions.castTypeParamMethods
 import net.noresttherein.sugar.exceptions.raise
-import net.noresttherein.sugar.optional.extensions.{OptionExtension, OptionCompanionExtension, ensuringMethodsConversion, ifTrueMethods, providingMethods, satisfyingMethods}
-import net.noresttherein.sugar.typist.PriorityConversion
+import net.noresttherein.sugar.optional.extensions.{OptionExtension, OptionCompanionExtension, ifTrueMethods, providingMethods, satisfyingMethods}
 import net.noresttherein.sugar.vars.{Fallible, Opt, Pill, Potential, Unsure}
 import net.noresttherein.sugar.vars.Fallible.{Failed, Passed}
 import net.noresttherein.sugar.vars.Pill.{Blue, Red}
@@ -33,11 +31,6 @@ trait extensions extends Any {
 	  */
 	@inline implicit final def providingMethods[T](self: => T) :providingMethods[T] = new providingMethods[T](self)
 
-	/** Adds additional `ensuring` methods to any object which accept exception classes to throw on failure. */
-	@inline implicit final def ensuringMethods[T] :ensuringMethodsConversion[T] =
-		extensions.ensuringMethodsConversionPrototype.castParam[T]
-
-//	@inline implicit final def ensuringMethods[T](self :T) :ensuringMethods[T] = new ensuringMethods[T](self)
 //duplicated in casting
 //	/** Adds `ifInstanceOf` and `ifSubclass` methods to any value providing `Option`-returning casting
 //	  * and optional execution of a function based on the argument's runtime class.
@@ -280,48 +273,6 @@ object extensions extends extensions {
 		@inline def unless(condition :Boolean) :Option[T] =
 			if (condition) None else Some(self)
 	}
-
-
-
-	//todo: new method name and a macro to include the actual expression used as a string.
-	/** Extension methods overloading `scala.ensuring`, allowing for passing an arbitrary exception to be thrown. */
-	class ensuringMethods[T](private val self :T) extends AnyVal {
-
-		/** Return `this` if `condition` is true, or throw the give exception otherwise. */
-		@inline def ensuring(condition :Boolean, ex :Exception) :T =
-			if (condition) self else throw ex
-
-
-		/** Return `this` if `condition` is false, or throw the given exception otherwise. */
-		@inline def ensuring(condition :T => Boolean, ex :Exception) :T =
-			if (condition(self)) self else throw ex
-
-
-		/** Return `this` if `condition` is true, or raise the exception given as type parameter (must provide
-		  * a default constructor). */
-		@inline def ensuring[E <: Exception :ClassTag](condition :Boolean) :T =
-			if (condition) self else raise[E]
-
-		/** Return `this` if `condition` is true, or raise the exception specified as the type parameter,
-		  * passing the given string to its constructor. */
-		@inline def ensuring[E <: Exception :ClassTag](condition :Boolean, msg: => String) :T =
-			if (condition) self else raise[E](msg)
-
-		//methods from Predef.Ensuring, because we are shadowing them
-	    def ensuring(cond: Boolean) :T = { assert(cond); self }
-	    def ensuring(cond: Boolean, msg: => Any) :T = { assert(cond, msg); self }
-	    def ensuring(cond: T => Boolean) :T = { assert(cond(self)); self }
-	    def ensuring(cond: T => Boolean, msg: => Any) :T = { assert(cond(self), msg); self }
-	}
-
-	sealed trait ensuringMethodsConversion[T] extends (T => ensuringMethods[T]) {
-		/* See collections.extensions.ArrayExtensionConversion. */
-		@inline final def apply(v1 :T)(implicit dummy :DummyImplicit) :ensuringMethods[T] = new ensuringMethods(v1)
-	}
-	private def newEnsuringMethodsConversion[T] =
-		new PriorityConversion.Wrapped[T, ensuringMethods[T]](new ensuringMethods(_))
-			with ensuringMethodsConversion[T]
-	private val ensuringMethodsConversionPrototype :ensuringMethodsConversion[Any] = newEnsuringMethodsConversion
 
 
 
--- src/main/scala/net/noresttherein/sugar/slang/extensions.scala
+++ src/main/scala/net/noresttherein/sugar/slang/extensions.scala
@@ -1,6 +1,11 @@
 package net.noresttherein.sugar.slang
 
-import net.noresttherein.sugar.slang.extensions.AnyRefExtension
+import scala.reflect.ClassTag
+
+import net.noresttherein.sugar.exceptions.raise
+import net.noresttherein.sugar.slang.extensions.{AnyRefExtension, ensuringMethodsConversion}
+import net.noresttherein.sugar.typist.PriorityConversion
+import net.noresttherein.sugar.typist.casting.extensions.castTypeParamMethods
 
 
 
@@ -9,6 +14,13 @@ import net.noresttherein.sugar.slang.extensions.AnyRefExtension
 trait extensions extends Any {
 	/** Adds `identityHashCode` method to all reference types. */
 	@inline implicit final def AnyRefExtension(self :AnyRef) :AnyRefExtension = new AnyRefExtension(self)
+
+	/** Adds additional `ensuring` methods to any object which accept exception classes to throw on failure. */
+	@inline implicit final def ensuringMethods[T] :ensuringMethodsConversion[T] =
+		extensions.ensuringMethodsConversionPrototype.castParam[T]
+
+//	@inline implicit final def ensuringMethods[T](self :T) :ensuringMethods[T] = new ensuringMethods[T](self)
+
 }
 
 
@@ -19,4 +31,47 @@ object extensions extends extensions {
 		/** The value of `hashCode` as it would be inherited for this object from the default implementation in `AnyRef`. */
 		@inline def identityHashCode :Int = System.identityHashCode(self)
 	}
+
+
+
+	//todo: new method name and a macro to include the actual expression used as a string.
+	/** Extension methods overloading `scala.ensuring`, allowing for passing an arbitrary exception to be thrown. */
+	class ensuringMethods[T](private val self :T) extends AnyVal {
+
+		/** Return `this` if `condition` is true, or throw the give exception otherwise. */
+		@inline def ensuring(condition :Boolean, ex :Exception) :T =
+			if (condition) self else throw ex
+
+
+		/** Return `this` if `condition` is false, or throw the given exception otherwise. */
+		@inline def ensuring(condition :T => Boolean, ex :Exception) :T =
+			if (condition(self)) self else throw ex
+
+
+		/** Return `this` if `condition` is true, or raise the exception given as type parameter (must provide
+		  * a default constructor). */
+		@inline def ensuring[E <: Exception :ClassTag](condition :Boolean) :T =
+			if (condition) self else raise[E]
+
+		/** Return `this` if `condition` is true, or raise the exception specified as the type parameter,
+		  * passing the given string to its constructor. */
+		@inline def ensuring[E <: Exception :ClassTag](condition :Boolean, msg: => String) :T =
+			if (condition) self else raise[E](msg)
+
+		//methods from Predef.Ensuring, because we are shadowing them
+	    def ensuring(cond: Boolean) :T = { assert(cond); self }
+	    def ensuring(cond: Boolean, msg: => Any) :T = { assert(cond, msg); self }
+	    def ensuring(cond: T => Boolean) :T = { assert(cond(self)); self }
+	    def ensuring(cond: T => Boolean, msg: => Any) :T = { assert(cond(self), msg); self }
+	}
+
+	sealed trait ensuringMethodsConversion[T] extends (T => ensuringMethods[T]) {
+		/* See collections.extensions.ArrayExtensionConversion. */
+		@inline final def apply(v1 :T)(implicit dummy :DummyImplicit) :ensuringMethods[T] = new ensuringMethods(v1)
+	}
+	private def newEnsuringMethodsConversion[T] =
+		new PriorityConversion.Wrapped[T, ensuringMethods[T]](new ensuringMethods(_))
+			with ensuringMethodsConversion[T]
+	private val ensuringMethodsConversionPrototype :ensuringMethodsConversion[Any] = newEnsuringMethodsConversion
+
 }
