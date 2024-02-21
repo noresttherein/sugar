f50f3fe5950e61b25e1712de3a7acac3f7a68329
noresttherein <marcin@moscicki.net>
2024-02-21 21:54:40 +0100

    cosmetic changes.
    cosmetic changes.

--- src/main/scala/net/noresttherein/sugar/collections/Views.scala
+++ src/main/scala/net/noresttherein/sugar/collections/Views.scala
@@ -33,26 +33,26 @@ private object Views {
 
 	@tailrec def zipEven[A, B](left :IterableOnce[A], right :IterableOnce[B]) :BaseView[(A, B)] =
 		left match {
-			case l :Iterable[A] => right match {
-				case r :Iterable[B] => () => Iterators.zipEven(l.iterator, r.iterator)
-				case _              => zipEven(l, TemporaryBuffer from right)
+			case _ :Iterable[A] => right match {
+				case _ :Iterable[B] => () => Iterators.zipEven(left.iterator, right.iterator)
+				case _              => zipEven(left, TemporaryBuffer from right)
 			}
 			case _ => zipEven(TemporaryBuffer from left, right)
 		}
 
 	@tailrec def zipMap[A, B, O](left :IterableOnce[A], right :IterableOnce[B])(f :(A, B) => O) :BaseView[O] =
 		left match {
-			case l :Iterable[A] => right match {
-				case r :Iterable[A] => () => Iterators.zipMap(l.iterator, r.iterator)(f)
-				case _              => zipMap(l, TemporaryBuffer from right)(f)
+			case _ :Iterable[A] => right match {
+				case _ :Iterable[B] => () => Iterators.zipMap(left.iterator, right.iterator)(f)
+				case _              => zipMap(left, TemporaryBuffer from right)(f)
 			}
 			case _ => zipMap(TemporaryBuffer from left, right)(f)
 		}
 
 	@tailrec def zipMapEven[A, B, O](left :IterableOnce[A], right :IterableOnce[B])(f :(A, B) => O) :BaseView[O] =
 		left match {
-			case l :Iterable[A] => right match {
-				case r :Iterable[A] => () => Iterators.zipMapEven(left.iterator, right.iterator)(f)
+			case _ :Iterable[A] => right match {
+				case _ :Iterable[B] => () => Iterators.zipMapEven(left.iterator, right.iterator)(f)
 				case _              => zipMapEven(left, TemporaryBuffer from right)(f)
 			}
 			case _ => zipMapEven(TemporaryBuffer from left, right)(f)
@@ -61,8 +61,8 @@ private object Views {
 	@tailrec def zipMapAll[A, B, O](left :IterableOnce[A], right :IterableOnce[B], leftElem :A, rightElem :B)
 	                               (f :(A, B) => O) :BaseView[O] =
 		left match {
-			case l :Iterable[A] => right match {
-				case r :Iterable[A] => () => Iterators.zipMapAll(left.iterator, right.iterator, leftElem, rightElem)(f)
+			case _ :Iterable[A] => right match {
+				case _ :Iterable[B] => () => Iterators.zipMapAll(left.iterator, right.iterator, leftElem, rightElem)(f)
 				case _              => zipMapAll(left, TemporaryBuffer from right, leftElem, rightElem)(f)
 			}
 			case _ => zipMapAll(TemporaryBuffer from left, right, leftElem, rightElem)(f)
@@ -71,9 +71,9 @@ private object Views {
 	@tailrec def zipFlatMap[A, B, O](left :IterableOnce[A], right :IterableOnce[B])
 	                                (f :(A, B) => IterableOnce[O]) :BaseView[O] =
 		left match {
-			case l :Iterable[A] => right match {
-				case r :Iterable[A] => () => Iterators.zipFlatMap(l.iterator, r.iterator)(f)
-				case _              => zipFlatMap(l, TemporaryBuffer from right)(f)
+			case _ :Iterable[A] => right match {
+				case _ :Iterable[B] => () => Iterators.zipFlatMap(left.iterator, right.iterator)(f)
+				case _              => zipFlatMap(left, TemporaryBuffer from right)(f)
 			}
 			case _ => zipFlatMap(TemporaryBuffer from left, right)(f)
 		}
@@ -81,18 +81,18 @@ private object Views {
 	@tailrec def zipFlatMapEven[A, B, O](left :IterableOnce[A], right :IterableOnce[B])
 	                                    (f :(A, B) => IterableOnce[O]) :BaseView[O] =
 		left match {
-			case l :Iterable[A] => right match {
-				case r :Iterable[A] => () => Iterators.zipFlatMapEven(left.iterator, right.iterator)(f)
+			case _ :Iterable[A] => right match {
+				case _ :Iterable[B] => () => Iterators.zipFlatMapEven(left.iterator, right.iterator)(f)
 				case _              => zipFlatMapEven(left, TemporaryBuffer from right)(f)
 			}
 			case _ => zipFlatMapEven(TemporaryBuffer from left, right)(f)
 		}
 
 	@tailrec def zipFlatMapAll[A, B, O](left :IterableOnce[A], right :IterableOnce[B], leftElem :A, rightElem :B)
-	                               (f :(A, B) => IterableOnce[O]) :BaseView[O] =
+	                                   (f :(A, B) => IterableOnce[O]) :BaseView[O] =
 		left match {
-			case l :Iterable[A] => right match {
-				case r :Iterable[A] =>
+			case _ :Iterable[A] => right match {
+				case _ :Iterable[B] =>
 					() => Iterators.zipFlatMapAll(left.iterator, right.iterator, leftElem, rightElem)(f)
 				case _              =>
 					zipFlatMapAll(left, TemporaryBuffer from right, leftElem, rightElem)(f)
@@ -103,10 +103,10 @@ private object Views {
 	@tailrec def zip3[A, B, C](first :IterableOnce[A], second :IterableOnce[B], third :IterableOnce[C])
 			:BaseView[(A, B, C)] =
 		first match {
-			case _1 :Iterable[A] => second match {
-				case _2 :Iterable[B] => third match {
-					case _3 :Iterable[C] => () => Iterators.zip3(_1.iterator, _2.iterator, _3.iterator)
-					case _               => zip3(first, second, TemporaryBuffer from third)
+			case _ :Iterable[A] => second match {
+				case _ :Iterable[B] => third match {
+					case _ :Iterable[C] => () => Iterators.zip3(first.iterator, second.iterator, third.iterator)
+					case _              => zip3(first, second, TemporaryBuffer from third)
 				}
 				case _ => zip3(first, TemporaryBuffer from second, third)
 			}
@@ -117,10 +117,10 @@ private object Views {
 	@tailrec def zipEven3[A, B, C](first :IterableOnce[A], second :IterableOnce[B], third :IterableOnce[C])
 			:BaseView[(A, B, C)] =
 		first match {
-			case _1 :Iterable[A] => second match {
-				case _2 :Iterable[B] => third match {
-					case _3 :Iterable[C] => () => Iterators.zipEven3(_1.iterator, _2.iterator, _3.iterator)
-					case _               => zipEven3(first, second, TemporaryBuffer from third)
+			case _ :Iterable[A] => second match {
+				case _ :Iterable[B] => third match {
+					case _ :Iterable[C] => () => Iterators.zipEven3(first.iterator, second.iterator, third.iterator)
+					case _              => zipEven3(first, second, TemporaryBuffer from third)
 				}
 				case _ => zipEven3(first, TemporaryBuffer from second, third)
 			}
@@ -131,11 +131,13 @@ private object Views {
 	@tailrec def zipAll3[A, B, C](first :IterableOnce[A], second :IterableOnce[B], third :IterableOnce[C],
 	                              firstElem :A, secondElem :B, thirdElem :C) :BaseView[(A, B, C)] =
 		first match {
-			case _1 :Iterable[A] => second match {
-				case _2 :Iterable[B] => third match {
-					case _3 :Iterable[C] =>
-						() => Iterators.zipAll3(_1.iterator, _2.iterator, _3.iterator, firstElem, secondElem, thirdElem)
-					case _               =>
+			case _ :Iterable[A] => second match {
+				case _ :Iterable[B] => third match {
+					case _ :Iterable[C] =>
+						() => Iterators.zipAll3(
+							first.iterator, second.iterator, third.iterator, firstElem, secondElem, thirdElem
+						)
+					case _              =>
 						zipAll3(first, second, TemporaryBuffer from third, firstElem, secondElem, thirdElem)
 				}
 				case _ => zipAll3(first, TemporaryBuffer from second, third, firstElem, secondElem, thirdElem)
@@ -145,110 +147,110 @@ private object Views {
 		}
 
 	@tailrec def zipTail[A](self :IterableOnce[A]) :BaseView[(A, A)] = self match {
-		case items :Iterable[A] => () => Iterators.zipTail(self.iterator)
-		case _                  => zipTail(TemporaryBuffer from self)
+		case _ :Iterable[A] => () => Iterators.zipTail(self.iterator)
+		case _              => zipTail(TemporaryBuffer from self)
 	}
 
 
 	@tailrec def mapWith[A, B, C](self :IterableOnce[A], z :B, f :(A, B) => (C, B)) :BaseView[C] = self match {
-		case items :Iterable[A] => () => Iterators.mapWith(items.iterator, z, f)
-		case _                  => mapWith(TemporaryBuffer from self, z, f)
+		case _ :Iterable[A] => () => Iterators.mapWith(self.iterator, z, f)
+		case _              => mapWith(TemporaryBuffer from self, z, f)
 	}
 
 	@tailrec def flatMapWith[A, B, C](self :IterableOnce[A], z :B, f :(A, B) => (IterableOnce[C], B)) :BaseView[C] =
 		self match {
-			case items :Iterable[A] => () => Iterators.flatMapWith(items.iterator, z, f)
-			case _                  => flatMapWith(TemporaryBuffer from self, z, f)
+			case _ :Iterable[A] => () => Iterators.flatMapWith(self.iterator, z, f)
+			case _              => flatMapWith(TemporaryBuffer from self, z, f)
 		}
 
 	@tailrec def mapWithIndex[A, B](self :IterableOnce[A], f :(A, Int) => B) :BaseView[B] =
 		self match {
-			case items :Iterable[A] => () => Iterators.mapWithIndex(items.iterator, f)
-			case _                  => mapWithIndex(TemporaryBuffer from self, f)
+			case _ :Iterable[A] => () => Iterators.mapWithIndex(self.iterator, f)
+			case _              => mapWithIndex(TemporaryBuffer from self, f)
 		}
 
 	@tailrec def flatMapWithIndex[A, B](self :IterableOnce[A], f :(A, Int) => IterableOnce[B]) :BaseView[B] =
 		self match {
-			case items :Iterable[A] => () => Iterators.flatMapWithIndex(items.iterator, f)
-			case _                  => flatMapWithIndex(TemporaryBuffer from self, f)
+			case _ :Iterable[A] => () => Iterators.flatMapWithIndex(self.iterator, f)
+			case _              => flatMapWithIndex(TemporaryBuffer from self, f)
 		}
 
 	@tailrec def collectWithIndex[A, B](self :IterableOnce[A], f :PartialFunction[(A, Int), B]) :BaseView[B] =
 		self match {
-			case items :Iterable[A] => () => Iterators.collectWithIndex(items.iterator, f)
-			case _                  => collectWithIndex(TemporaryBuffer from self, f)
+			case _ :Iterable[A] => () => Iterators.collectWithIndex(self.iterator, f)
+			case _              => collectWithIndex(TemporaryBuffer from self, f)
 		}
 
 	@tailrec def mapWhile[A, B, C](self :IterableOnce[A], z :B, pred :B => Boolean, f :(B, A) => (B, C)) :BaseView[C] =
 		self match {
-			case items :Iterable[A] => () => Iterators.mapWhile(items.iterator, z, pred, f)
-			case _                  => mapWhile(TemporaryBuffer from self, z, pred, f)
+			case _ :Iterable[A] => () => Iterators.mapWhile(self.iterator, z, pred, f)
+			case _              => mapWhile(TemporaryBuffer from self, z, pred, f)
 		}
 
 	@tailrec def flatMapWhile[A, B, C](self :IterableOnce[A], z :B, pred :B => Boolean, f :(B, A) => (B, IterableOnce[C]))
 			:BaseView[C] =
 		self match {
-			case items :Iterable[A] => () => Iterators.flatMapWhile(items.iterator, z, pred, f)
-			case _                  => flatMapWhile(TemporaryBuffer from self, z, pred, f)
+			case _ :Iterable[A] => () => Iterators.flatMapWhile(self.iterator, z, pred, f)
+			case _              => flatMapWhile(TemporaryBuffer from self, z, pred, f)
 		}
 
 	@tailrec def mapUntil[A, B, C](self :IterableOnce[A], z :B, f :(B, A) => (Boolean, B, C)) :BaseView[C] =
 		self match {
-			case items :Iterable[A] => () => Iterators.mapUntil(items.iterator, z, f)
-			case _                  => mapUntil(TemporaryBuffer from self, z, f)
+			case _ :Iterable[A] => () => Iterators.mapUntil(self.iterator, z, f)
+			case _              => mapUntil(TemporaryBuffer from self, z, f)
 		}
 
 	@tailrec def flatMapUntil[A, B, C](self :IterableOnce[A], z :B, f :(B, A) => (Boolean, B, IterableOnce[C]))
 			:BaseView[C] =
 		self match {
-			case items :Iterable[A] => () => Iterators.flatMapUntil(items.iterator, z, f)
-			case _                  => flatMapUntil(TemporaryBuffer from self, z, f)
+			case _ :Iterable[A] => () => Iterators.flatMapUntil(self.iterator, z, f)
+			case _              => flatMapUntil(TemporaryBuffer from self, z, f)
 		}
 
 	@tailrec def mapSome[A, B, C](self :IterableOnce[A], z :B, f :(B, A) => Option[(B, C)]) :BaseView[C] =
 		self match {
-			case items :Iterable[A] => () => Iterators.mapSome(items.iterator, z, f)
-			case _                  => mapSome(TemporaryBuffer from self, z, f)
+			case _ :Iterable[A] => () => Iterators.mapSome(self.iterator, z, f)
+			case _              => mapSome(TemporaryBuffer from self, z, f)
 		}
 
 	@tailrec def flatMapSome[A, B, C](self :IterableOnce[A], z :B, f :(B, A) => Option[(B, IterableOnce[C])])
 			:BaseView[C] =
 		self match {
-			case items :Iterable[A] => () => Iterators.flatMapSome(items.iterator, z, f)
-			case _                  => flatMapSome(TemporaryBuffer from self, z, f)
+			case _ :Iterable[A] => () => Iterators.flatMapSome(self.iterator, z, f)
+			case _              => flatMapSome(TemporaryBuffer from self, z, f)
 		}
 
 	@tailrec def mapPrefix[A, B, C](self :IterableOnce[A], z :B, f :(B, A) => Opt[(B, C)]) :BaseView[C] =
 		self match {
-			case items :Iterable[A] => () => Iterators.mapPrefix(items.iterator, z, f)
-			case _                  => mapPrefix(TemporaryBuffer from self, z, f)
+			case _ :Iterable[A] => () => Iterators.mapPrefix(self.iterator, z, f)
+			case _              => mapPrefix(TemporaryBuffer from self, z, f)
 		}
 
 	@tailrec def flatMapPrefix[A, B, C](self :IterableOnce[A], z :B, f :(B, A) => Opt[(B, IterableOnce[C])])
 			:BaseView[C] =
 		self match {
-			case items :Iterable[A] => () => Iterators.flatMapPrefix(items.iterator, z, f)
-			case _                  => flatMapPrefix(TemporaryBuffer from self, z, f)
+			case _ :Iterable[A] => () => Iterators.flatMapPrefix(self.iterator, z, f)
+			case _              => flatMapPrefix(TemporaryBuffer from self, z, f)
 		}
 
 
 	@tailrec def filterWith[A, B](self :IterableOnce[A], z :B, pred :(A, B) => (Boolean, B), keep :Boolean) :BaseView[A] =
 		self match {
-			case items :Iterable[A] => () => Iterators.filterWith(items.iterator, z, pred, keep)
-			case _                  => filterWith(TemporaryBuffer from self, z, pred, keep)
+			case _ :Iterable[A] => () => Iterators.filterWith(self.iterator, z, pred, keep)
+			case _              => filterWith(TemporaryBuffer from self, z, pred, keep)
 		}
 
 	@tailrec def filterWithIndex[A](self :IterableOnce[A], pred :(A, Int) => Boolean, keep :Boolean) :BaseView[A] =
 		self match {
-			case items :Iterable[A] => () => Iterators.filterWithIndex(items.iterator, pred, keep)
-			case _                  => filterWithIndex(TemporaryBuffer from self, pred, keep)
+			case _ :Iterable[A] => () => Iterators.filterWithIndex(self.iterator, pred, keep)
+			case _              => filterWithIndex(TemporaryBuffer from self, pred, keep)
 		}
 
 	@tailrec def keep[A](self :IterableOnce[A], pred :Int => Boolean) :View[A] =
 		self match {
 			case _ if self.knownSize == 0 => View.empty
-			case items :Iterable[A] => (() => Iterators.keep(items.iterator, pred)) :BaseView[A]
-			case _                  => keep(TemporaryBuffer from self, pred)
+			case _ :Iterable[A] => (() => Iterators.keep(self.iterator, pred)) :BaseView[A]
+			case _              => keep(TemporaryBuffer from self, pred)
 		}
 
 	@tailrec def distinct[A](self :IterableOnce[A]) :View[A] = {
