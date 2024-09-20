71a55b0bb166f865dd26b9df72eb5592cb232a4c
noresttherein <marcin@moscicki.net>
2024-09-21 01:10:38 +0200

    casting.nullAs

--- src/main/scala/net/noresttherein/sugar/casting/casting.scala
+++ src/main/scala/net/noresttherein/sugar/casting/casting.scala
@@ -5,4 +5,7 @@ import net.noresttherein.sugar
 
 package object casting extends extensions {
 	private[casting] final val Ver = sugar.Ver
+
+	/** Casts `null` to `X`. */
+	@inline def nullAs[X] :X = null.asInstanceOf[X]
 }
