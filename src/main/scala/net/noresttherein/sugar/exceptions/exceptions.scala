696b59886457a4530c882402d82f77fa48f3803c
noresttherein <marcin@moscicki.net>
2025-04-10 08:13:39 +0200

    Scaladoc for exception factory objects.

--- src/main/scala/net/noresttherein/sugar/exceptions/exceptions.scala
+++ src/main/scala/net/noresttherein/sugar/exceptions/exceptions.scala
@@ -51,6 +51,11 @@ package exceptions {
 
 
 
+/** A home to [[net.noresttherein.sugar.exceptions.SugaredThrowable SugaredThrowable]] subtrait of `Throwable`
+  * and its subclasses. Contains also [[net.noresttherein.sugar.exceptions.extensions extension]] methods
+  * for regular exceptions.
+  * @define factoryInfo A factory of '[[net.noresttherein.sugar.exceptions.SugaredException sugared]]' subclass of $class.
+  */
 package object exceptions extends exceptions.imports with exceptions.markerStackTraceElements {
 	private[exceptions] final val Ver = 1L
 
@@ -61,53 +66,89 @@ package object exceptions extends exceptions.imports with exceptions.markerStack
 	type LazyDetailedExceptionFactory     = LazyDetailedThrowableFactory[Exception]
 	type FlexibleDetailedExceptionFactory = FlexibleDetailedThrowableFactory[Exception]
 
+	type EagerRuntimeExceptionFactory            = EagerThrowableFactory[RuntimeException]
+	type LazyRuntimeExceptionFactory             = LazyThrowableFactory[RuntimeException]
+	type FlexibleRuntimeExceptionFactory         = FlexibleThrowableFactory[RuntimeException]
+	type EagerDetailedRuntimeExceptionFactory    = EagerDetailedThrowableFactory[RuntimeException]
+	type LazyDetailedRuntimeExceptionFactory     = LazyDetailedThrowableFactory[RuntimeException]
+	type FlexibleDetailedRuntimeExceptionFactory = FlexibleDetailedThrowableFactory[RuntimeException]
+
 	//todo: factories for other exception, together with methods in imports.
+	/** A factory of [[net.noresttherein.sugar.exceptions.Oops! Oops]] exceptions.*/
 	val Oops :FlexibleThrowableFactory[Oops] = ThrowableFactory("Oops", new Oops(_, _, _))
 
+	/** A factory of [[net.noresttherein.sugar.exceptions.ImpossibleError! ImpossibleError]] errors.*/
 	val ImpossibleError :FlexibleThrowableFactory[ImpossibleError] =
 		ThrowableFactory("This location should have been impossible to reach.", new ImpossibleError(_, _, _))
 
+	/** A factory of [[net.noresttherein.sugar.exceptions.MaxSizeReachedException! MaxSizedReachedException]] exceptions.*/
 	val MaxSizeReachedException :FlexibleExceptionFactory = //Consider: making it a lazy factory
 		ThrowableFactory("Size limit exceeded", new MaxSizeReachedException(_, _, _))
 
+	/** A factory of [[net.noresttherein.sugar.exceptions.IncompatibleArgumentsException! IncompatibleArgumentsException]]
+	  * subclass of [[IllegalArgumentException]].*/
 	val IncompatibleArgumentsException :LazyExceptionFactory = ThrowableFactory(
 		"Arguments do not satisfy method constraints.", new IncompatibleArgumentsException(null, _, _)
 	)
 
+	/** A factory of [[net.noresttherein.sugar.exceptions.IncompatibleArgumentTypesException! IncompatibleArgumentTypesException]]
+	  * subclass of [[IllegalArgumentException]].*/
 	val IncompatibleArgumentTypesException :LazyExceptionFactory = ThrowableFactory(
 		"Argument types do not satisfy method constraints.", new IncompatibleArgumentTypesException(null, _, _)
 	)
 
+	/** $factoryInfo
+	  * @define class `ArithmeticException` */
 	val SugaredArithmeticException :FlexibleExceptionFactory =
 		ThrowableFactory(new SugaredArithmeticException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `ClassCastException` */
 	val SugaredClassCastException :FlexibleExceptionFactory =
 		ThrowableFactory("Unexpected class.", new SugaredClassCastException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `ConcurrentModificationException` */
 	val SugaredConcurrentModificationException :FlexibleExceptionFactory =
 		ThrowableFactory("Concurrent modification.", new SugaredConcurrentModificationException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `IllegalArgumentException` */
 	val SugaredIllegalArgumentException :FlexibleExceptionFactory =
 		ThrowableFactory("Illegal argument.", new SugaredIllegalArgumentException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `IllegalStateException` */
 	val SugaredIllegalStateException :FlexibleExceptionFactory =
 		ThrowableFactory(new SugaredIllegalStateException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `IndexOutOfBoundsException` */
 	val SugaredIndexOutOfBoundsException :FlexibleExceptionFactory =
 		ThrowableFactory("Index out of bounds.", new SugaredIndexOutOfBoundsException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `IOException` */
 	val SugaredIOException :FlexibleExceptionFactory =
 		ThrowableFactory(new SugaredIOException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `NoSuchElementException` */
 	val SugaredNoSuchElementException :FlexibleExceptionFactory =
 		ThrowableFactory("No such element.", new SugaredNoSuchElementException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `NullPointerException` */
 	val SugaredNullPointerException :FlexibleExceptionFactory =
 		ThrowableFactory("null", new SugaredNullPointerException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `NumberFormatException` */
 	val SugaredNumberFormatException :FlexibleExceptionFactory =
 		ThrowableFactory("Invalid format.", new SugaredNumberFormatException(_, _, _))
 
+	/** $factoryInfo
+	  * @define class `UnsupportedOperationException` */
 	val SugaredUnsupportedOperationException :FlexibleExceptionFactory =
 		ThrowableFactory("Unsupported operation.", new SugaredUnsupportedOperationException(_, _, _))
 
